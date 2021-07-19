53a573c9440f7ef0dd71f8c8524ff1e589dfbf0c
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-07-19 16:25:44 +0300

    added nosql reader to autofachepler

--- src/Service.PriceHistory.Client/AutofacHelper.cs
+++ src/Service.PriceHistory.Client/AutofacHelper.cs
@@ -1,4 +1,7 @@
 ﻿using Autofac;
+using MyJetWallet.Sdk.NoSql;
+using MyNoSqlServer.DataReader;
+using Service.PriceHistory.Domain.NoSql;
 using Service.PriceHistory.Grpc;
 
 // ReSharper disable UnusedMember.Global
@@ -12,6 +15,12 @@ namespace Service.PriceHistory.Client
             var factory = new PriceHistoryClientFactory(grpcServiceUrl);
 
             builder.RegisterInstance(factory.GetBasePriceService()).As<IBasePriceSerivce>().SingleInstance();
+        }
+
+        public static void RegisterPriceHistoryNoSqlClient(this ContainerBuilder builder, MyNoSqlTcpClient client)
+        {
+            builder.RegisterMyNoSqlReader<InstrumentPriceRecordNoSqlEntity>(client,
+                InstrumentPriceRecordNoSqlEntity.TableName);
         }
     }
 }
