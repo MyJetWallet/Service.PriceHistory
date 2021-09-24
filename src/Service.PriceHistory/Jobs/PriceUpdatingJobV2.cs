237817403f10c32104ea3977f72e8b1b67653e0a
Alexey <alexey.p@smplt.net>
2021-09-24 17:32:37 +0300

    add round prices

--- src/Service.PriceHistory/Jobs/PriceUpdatingJobV2.cs
+++ src/Service.PriceHistory/Jobs/PriceUpdatingJobV2.cs
@@ -8,6 +8,7 @@ using MyJetWallet.Domain;
 using MyJetWallet.Sdk.Service.Tools;
 using MyNoSqlServer.Abstractions;
 using Service.AssetsDictionary.Client;
+using Service.AssetsDictionary.Domain.Models;
 using Service.BaseCurrencyConverter.Grpc;
 using Service.BaseCurrencyConverter.Grpc.Models;
 using Service.PriceHistory.Domain.Models;
@@ -24,7 +25,7 @@ namespace Service.PriceHistory.Jobs
         private readonly IMyNoSqlServerDataWriter<AssetPriceRecordNoSqlEntity> _assetPriceRecordDataWriter;
         private readonly IMyNoSqlServerDataWriter<AssetPricesNoSqlEntity> _assetPricesDataWriter;
 
-        private List<string> _assets = new();
+        private List<IAsset> _assets = new();
 
         public PriceUpdatingJobV2(ILogger<PriceUpdatingJobV2> logger,
             IBaseCurrencyConverterService baseCurrencyConverterService,
@@ -57,14 +58,14 @@ namespace Service.PriceHistory.Jobs
                 {
                     var assetPrices = new AssetPrices()
                     {
-                        BaseAsset = baseAsset,
+                        BaseAsset = baseAsset.Symbol,
                         BrokerId = DomainConstants.DefaultBroker,
                         CalculatingTime = DateTime.UtcNow,
                         PricesByQuoteAsset = new List<AssetPrice>()
                     };
                     var convertMap = await _baseCurrencyConverterService.GetConvertorMapToBaseCurrencyAsync(new GetConvertorMapToBaseCurrencyRequest()
                     {
-                        BaseAsset = baseAsset,
+                        BaseAsset = baseAsset.Symbol,
                         BrokerId = DomainConstants.DefaultBroker
                     });
                     foreach (var quoteAsset in _assets)
@@ -72,7 +73,7 @@ namespace Service.PriceHistory.Jobs
                         var startValue = 1m;
                         var priceByQuoteAsset = new AssetPrice()
                         {
-                            Asset = quoteAsset,
+                            Asset = quoteAsset.Symbol,
                             CurrentPrice = startValue,
                             H24 = startValue,
                             D7 = startValue,
@@ -82,7 +83,7 @@ namespace Service.PriceHistory.Jobs
                         if (baseAsset == quoteAsset)
                         {
                             var pricesByOperationSymbol = pricesByOperationSymbols.FirstOrDefault(e =>
-                                e.AssetPriceRecord.AssetSymbol == quoteAsset)?.AssetPriceRecord;
+                                e.AssetPriceRecord.AssetSymbol == quoteAsset.Symbol)?.AssetPriceRecord;
                             
                             priceByQuoteAsset.CurrentPrice = pricesByOperationSymbol.CurrentPrice;
                             priceByQuoteAsset.H24 = pricesByOperationSymbol.H24.Price;
@@ -92,7 +93,7 @@ namespace Service.PriceHistory.Jobs
                         }
                         else
                         {
-                            var map = convertMap.Maps.FirstOrDefault(e => e.AssetSymbol == quoteAsset);
+                            var map = convertMap.Maps.FirstOrDefault(e => e.AssetSymbol == quoteAsset.Symbol);
                             if (map != null)
                             {
                                 foreach (var operation in map.Operations)
@@ -122,6 +123,7 @@ namespace Service.PriceHistory.Jobs
                                 }
                             }
                         }
+                        RoundPrices(priceByQuoteAsset, baseAsset.Accuracy);
                         assetPrices.PricesByQuoteAsset.Add(priceByQuoteAsset);
                     }
                     await _assetPricesDataWriter.InsertOrReplaceAsync(AssetPricesNoSqlEntity.Create(assetPrices));
@@ -132,14 +134,24 @@ namespace Service.PriceHistory.Jobs
                 _logger.LogError(ex, ex.Message);
             }
         }
+
+        private void RoundPrices(AssetPrice priceByQuoteAsset, int baseAssetAccuracy)
+        {
+            priceByQuoteAsset.CurrentPrice = Math.Round(priceByQuoteAsset.CurrentPrice, baseAssetAccuracy);
+            priceByQuoteAsset.H24 = Math.Round(priceByQuoteAsset.H24, baseAssetAccuracy);
+            priceByQuoteAsset.D7 = Math.Round(priceByQuoteAsset.D7, baseAssetAccuracy);
+            priceByQuoteAsset.M1 = Math.Round(priceByQuoteAsset.M1, baseAssetAccuracy);
+            priceByQuoteAsset.M3 = Math.Round(priceByQuoteAsset.M3, baseAssetAccuracy);
+        }
+
         private async Task UpdateAssets()
         {
-            var assetList = _assetsDictionaryClient.GetAllAssets().Where(e => e.IsEnabled).Select(e => e.Symbol).Distinct().ToList();
+            var assetList = _assetsDictionaryClient.GetAllAssets().Where(e => e.IsEnabled).ToList();
 
             if (!assetList.Any())
             {
                 await Task.Delay(5000);
-                assetList = _assetsDictionaryClient.GetAllAssets().Where(e => e.IsEnabled).Select(e => e.Symbol).Distinct().ToList();
+                assetList = _assetsDictionaryClient.GetAllAssets().Where(e => e.IsEnabled).ToList();
             }
             _assets.Clear();
             _assets = assetList;
